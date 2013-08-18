9e575d3e63c913f794658055d29798a2b2cf5fd7
Shawn Melton <shawn.a.melton@gmail.com>
2013-08-17 21:58:37 -0400

    Customizing.

--- request.php
+++ request.php
@@ -1,11 +1,6 @@
 <?php
 require_once 'TwitterClient.php';
-
-// fill in your keys - https://dev.twitter.com/apps/new
-define ('CONSUMER_KEY',       '');
-define ('CONSUMER_SECRET',    '');
-define ('OAUTH_TOKEN',        '');
-define ('OAUTH_TOKEN_SECRET', '');
+require_once 'config.php';
 
 $twitter = new TwitterClient(CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET);
 $twitter->setResponseFormat('json'); // optional, json is default
@@ -18,16 +13,9 @@ var_dump($response);
 
 // https://dev.twitter.com/docs/api/1/get/statuses/user_timeline
 $params = array(
-    'count' => 10
+    'count' => 10,
+    'screen_name' => 'FergusonCareers'
 );
 $raw_response = $twitter->get('statuses/user_timeline', $params);
-$response = json_decode($raw_response, true);
-var_dump($response);
-
-// https://dev.twitter.com/docs/api/1/post/statuses/update
-$params = array(
-    'status' => 'I posted this with the @chucklarge php twitter reset api client. http://bit.ly/A8SQwg',
-);
-$raw_response = $twitter->post('statuses/update', $params);
 $response = json_decode($raw_response, true);
 var_dump($response);
